3f93fdd0bb2518a90ee6685fd747e34ad74f09b9
E0058193 <zied.khiari@soprasteria.com>
2025-08-04 18:31:23 +0200

    add activate disate sound video button when i call user

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -720,6 +720,21 @@ function toggleMute() {
                 muteBtn.classList.toggle('bg-gray-600', !isMuted);
                 muteBtn.title = isMuted ? 'Unmute' : 'Mute';
             }
+            
+            // Update mic status icon
+            const micStatus = document.getElementById('micStatus');
+            if (micStatus) {
+                const svg = micStatus.querySelector('svg');
+                if (svg) {
+                    if (isMuted) {
+                        // Muted icon - add slash
+                        svg.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clip-rule="evenodd"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2"></path>';
+                    } else {
+                        // Unmuted icon - original path
+                        svg.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"></path>';
+                    }
+                }
+            }
         }
     }
 }
